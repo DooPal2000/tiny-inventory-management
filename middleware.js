b0223b74684aa2cd6ddb4d6eea12dc0e89d10762
Kyoung Yeob <kkyub1313@gmail.com>
2024-12-04 13:37:37 +0900

    update(middleware): 미들웨어 리팩토링 (next 위치 조정)

--- middleware.js
+++ middleware.js
@@ -13,29 +13,29 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 
 module.exports.isAuthorized = (req, res, next) => {
-    if (req.isAuthenticated() && req.user.isActive) {
-        return next();
+    if (!req.isAuthenticated() || !req.user.isActive) {
+        req.flash('error', '활성화된 고객님이 아닙니다. 관리자에게 문의해 주세요.');
+        return res.redirect('/login');
     }
-    req.flash('error', '활성화된 고객님이 아닙니다. 관리자에게 문의해 주세요.');
-    res.redirect('/login');
+    next();
 };
 
+
 module.exports.isAdmin = (req, res, next) => {
-    if (req.isAuthenticated() && req.user.role === 'admin') {
-        return next();
+    if (!req.isAuthenticated() || req.user.role !== 'admin') {
+        req.flash('error', '관리자가 아닙니다.');
+        return res.redirect('/login');
     }
-    req.flash('error', '로그인 해 주세요.')
-    throw new ExpressError('관리자가 아닙니다.', 401);
+    next();
 };
 
 module.exports.isSecureAdminCreation = (req, res, next) => {
-    // 여기에 보안 로직 구현 (예: 특정 IP에서만 접근 가능, 특별한 토큰 필요 등)
     const userPhoneNumber = req.body.phonenum;
     const adminNumbers = JSON.parse(process.env.ADMIN_NUMBERS);
 
-
-    if (Object.values(adminNumbers).includes(userPhoneNumber)) {
-        return next();
+    if (!Object.values(adminNumbers).includes(userPhoneNumber)) {
+        req.flash('error', '관리자 핸드폰 번호가 아닙니다.');
+        return res.redirect('/unauthorized');
     }
-    throw new ExpressError('관리자 핸드폰 번호가 아닙니다.', 401);
-}
+    next();
+};

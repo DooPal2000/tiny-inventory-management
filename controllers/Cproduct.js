0b83f181aa1d1cb3b5130f6827383edffac2a7b7
Kyoung Yeob <kkyub1313@gmail.com>
2024-12-16 00:03:19 +0900

    feat(controllers): index.ejs 수량 변경 로직 작성

--- controllers/Cproduct.js
+++ controllers/Cproduct.js
@@ -16,11 +16,7 @@ module.exports.createProduct = async (req, res) => {
     product.createdBy = req.user._id;
     await product.save();
 
-    // 저장한 후 createdBy가 req.user._id인 모든 product를 가져옴
-    const products = await Product.find({ createdBy: req.user._id });
-
-    // products를 index 페이지로 렌더링
-    res.render('products/index', { products });
+    res.redirect('/products');
 };
 
 module.exports.showProduct = async (req, res) => {
@@ -40,9 +36,30 @@ module.exports.updateProduct = async (req, res) => {
     // 저장한 후 createdBy가 req.user._id인 모든 product를 가져옴
     const products = await Product.find({ createdBy: req.user._id });
 
-    // products를 index 페이지로 렌더링
-    res.render('products/index', { products });
+    res.redirect('/products');
 };
+
+
+module.exports.updateQuantity = async (req, res, next) => {
+    const { id } = req.params;
+    const { quantity } = req.body;
+
+    // 유효성 검사
+    if (typeof quantity !== 'number' || quantity < 0 || !Number.isInteger(quantity)) {
+        throw new ExpressError('수량은 0 이상의 정수여야 합니다.', 400);
+    }
+
+    const product = await Product.findById(id);
+    if (!product) {
+        throw new ExpressError('상품을 찾을 수 없습니다.', 404);
+    }
+
+    product.quantity = quantity;
+    await product.save();
+
+    res.json({ success: true, quantity: product.quantity });
+};
+
 
 module.exports.deleteProduct = async (req, res) => {
     const { id } = req.params;

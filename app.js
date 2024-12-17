df52be9353537095a1a02807266bbe90efaecacf
Kyoung Yeob <kkyub1313@gmail.com>
2024-12-17 17:30:23 +0900

    update(views): edit 삭제 버튼 추가

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 1,
+    maxAge: 1000 * 60 * 60 * 24 * 1
   }
 }
 app.use(session(sessionConfig));
